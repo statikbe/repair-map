3d9f1c135efbffd317e96bb2875dfb7836c36df7
Niels <niels@statik.be>
2021-10-22 17:38:02 +0200

    Fix NL content

--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -1,11 +1,11 @@
 export default {
   create_new_cta: 'Stel een nieuw repair initiatief voor',
   create_new_text:
-    'Het is ons doel om de grote diversiteit aan spelers in het domein van repair voor te stellen op deze kaart. Ken jij een repair café, professionele hersteller, een makerspace of een ander initiatief in je buurt? Laat het ons weten.',
+    'Het is ons doel om de grote diversiteit aan spelers in het domein van repair voor te stellen op deze kaart. Ken jij een Repair Café, professionele hersteller, een makerspace of een ander initiatief in je buurt? Laat het ons weten.',
   create_new_title: 'Ken je een repair initiatief dat hier nog niet bij staat?',
   filter_category_label: 'Categorie',
   filter_category_text:
-    "Elke herstelorganisatie heeft zijn eigen specialiteiten, do's en dont's. Filter de lijst op de productcategorie waarvoor u een herstelorganisatie in uw regio wilt vinden.",
+    "Elke hersteloptie heeft zijn eigen specialiteiten, do's en dont's. Filter de lijst op de productcategorie waarvoor u een hersteloptie in uw regio wilt vinden.",
   filter_category_title: 'Selecteer de productcategorie',
   filter_location_label: 'Locatie',
   filter_location_search_label: 'Geef je postcode of gemeente in',
@@ -13,12 +13,12 @@ export default {
   filter_location_title: 'Zoek naar locaties in jouw buurt',
   filter_type_label: 'Type',
   filter_type_text:
-    'Verschillende soorten organisaties dragen bij aan herstel, elk met een eigen aanpak en focus. Afhankelijk van het probleem dat je hebt, kun je ervoor kiezen om iets te laten repareren bij een professionele reparatiewerkplaats of samen met iemand in een repair café.',
-  filter_type_title: 'Selecteer de soorten organisaties van uw keuze',
+    'Verschillende soorten organisaties dragen bij aan herstel, elk met een eigen aanpak en focus. Afhankelijk van het probleem dat je hebt, kun je ervoor kiezen om iets te laten repareren bij een professionele reparatiewerkplaats of samen met iemand in een Repair Café.',
+  filter_type_title: 'Selecteer de soorten herstelopties van uw keuze',
   label_search_by: 'Zoeken op:',
   locations_results_0:
     '0 zoekresultaten gevonden in jouw regio. Er zijn helaas nog geen repair initiatieven geregistreerd op jouw locatie. Je kan wat uitzoomen op de kaart om spelers terug te vinden die iets verder van je verwijderd zijn, of zelf een nieuw initatief toevoegen aan de kaart via de link onderaan deze pagina.',
   locations_results_n: '{n} zoekresultat(en) gevonden in jouw regio.',
   location_name_fallback: 'Naamloze locatie',
-  page_title: 'Vind interessante herstelorganisaties in uw regio.',
+  page_title: 'Vind interessante herstelopties in uw regio.',
 };
