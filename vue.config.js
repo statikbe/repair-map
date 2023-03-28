1d20157a7eef45fa95a178c077ced2326acdca21
Kobe <kobe@statik.be>
2023-03-28 14:11:08 +0200

    REPMAP-141 bugfix embed build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.REPAIR_MAP_BASE_ROUTE || '',
+  publicPath: process.env.VUE_APP_BASE_ROUTE || '',
   // configureWebpack: {
   //   output: {
   //     libraryExport: 'default'
