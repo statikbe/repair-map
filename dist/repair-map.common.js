0e71a03c377e3354445f520473929c37ba36c418
Kobe <kobe@statik.be>
2024-03-12 16:57:44 +0100

    REPMAP-181 bugfix debug filter option

--- dist/repair-map.common.js
+++ dist/repair-map.common.js
@@ -707,17 +707,6 @@ module.exports = function (obj) {
 
 /***/ }),
 
-/***/ "095e":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_OrdpMap_vue_vue_type_style_index_4_id_2f44d178_prod_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("7795");
-/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_OrdpMap_vue_vue_type_style_index_4_id_2f44d178_prod_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_OrdpMap_vue_vue_type_style_index_4_id_2f44d178_prod_lang_css___WEBPACK_IMPORTED_MODULE_0__);
-/* unused harmony reexport * */
-
-
-/***/ }),
-
 /***/ "0a06":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -13831,22 +13820,6 @@ exports.f = Object.getOwnPropertySymbols;
 
 /***/ }),
 
-/***/ "7795":
-/***/ (function(module, exports, __webpack_require__) {
-
-// style-loader: Adds some css to the DOM by adding a <style> tag
-
-// load the styles
-var content = __webpack_require__("f8c1");
-if(content.__esModule) content = content.default;
-if(typeof content === 'string') content = [[module.i, content, '']];
-if(content.locals) module.exports = content.locals;
-// add the styles to the DOM
-var add = __webpack_require__("499e").default
-var update = add("6688c9ce", content, true, {"sourceMap":false,"shadowMode":false});
-
-/***/ }),
-
 /***/ "7839":
 /***/ (function(module, exports) {
 
@@ -14019,6 +13992,20 @@ if(content.locals) module.exports = content.locals;
 // add the styles to the DOM
 var add = __webpack_require__("499e").default
 var update = add("10ec2cd8", content, true, {"sourceMap":false,"shadowMode":false});
+
+/***/ }),
+
+/***/ "838c":
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__("24fb");
+exports = ___CSS_LOADER_API_IMPORT___(false);
+// Module
+exports.push([module.i, ".category-group>label span{font-weight:700}", ""]);
+// Exports
+module.exports = exports;
+
 
 /***/ }),
 
@@ -15935,6 +15922,17 @@ var invariant$1 = invariant;
 //# sourceMappingURL=invariant.esm.js.map
 
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__("4362")))
+
+/***/ }),
+
+/***/ "aabb":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_OrdpMap_vue_vue_type_style_index_4_id_d6b92486_prod_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("eb63");
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_OrdpMap_vue_vue_type_style_index_4_id_d6b92486_prod_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_OrdpMap_vue_vue_type_style_index_4_id_d6b92486_prod_lang_css___WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+
 
 /***/ }),
 
@@ -92616,6 +92614,22 @@ module.exports = Array.isArray || function isArray(argument) {
 
 /***/ }),
 
+/***/ "eb63":
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__("838c");
+if(content.__esModule) content = content.default;
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add the styles to the DOM
+var add = __webpack_require__("499e").default
+var update = add("0e77c032", content, true, {"sourceMap":false,"shadowMode":false});
+
+/***/ }),
+
 /***/ "ec74":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -93168,20 +93182,6 @@ module.exports = debounce;
 /* harmony import */ var _vue_style_loader_index_js_ref_7_oneOf_1_0_css_loader_dist_cjs_js_ref_7_oneOf_1_1_vue_loader_lib_loaders_stylePostLoader_js_postcss_loader_src_index_js_ref_7_oneOf_1_2_postcss_loader_src_index_js_ref_7_oneOf_1_3_MarkerCluster_css_vue_type_style_index_1_prod_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("19fb");
 /* harmony import */ var _vue_style_loader_index_js_ref_7_oneOf_1_0_css_loader_dist_cjs_js_ref_7_oneOf_1_1_vue_loader_lib_loaders_stylePostLoader_js_postcss_loader_src_index_js_ref_7_oneOf_1_2_postcss_loader_src_index_js_ref_7_oneOf_1_3_MarkerCluster_css_vue_type_style_index_1_prod_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_vue_style_loader_index_js_ref_7_oneOf_1_0_css_loader_dist_cjs_js_ref_7_oneOf_1_1_vue_loader_lib_loaders_stylePostLoader_js_postcss_loader_src_index_js_ref_7_oneOf_1_2_postcss_loader_src_index_js_ref_7_oneOf_1_3_MarkerCluster_css_vue_type_style_index_1_prod_lang_css___WEBPACK_IMPORTED_MODULE_0__);
 /* unused harmony reexport * */
-
-
-/***/ }),
-
-/***/ "f8c1":
-/***/ (function(module, exports, __webpack_require__) {
-
-// Imports
-var ___CSS_LOADER_API_IMPORT___ = __webpack_require__("24fb");
-exports = ___CSS_LOADER_API_IMPORT___(false);
-// Module
-exports.push([module.i, ".category-group>label span{font-weight:700}", ""]);
-// Exports
-module.exports = exports;
 
 
 /***/ }),
@@ -94488,8 +94488,8 @@ var RepairConnectsMap_component = normalizeComponent(
 )
 
 /* harmony default export */ var RepairConnectsMap = (RepairConnectsMap_component.exports);
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"8c1e7290-vue-loader-template"}!./node_modules/cache-loader/dist/cjs.js??ref--13-0!./node_modules/babel-loader/lib!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./src/OrdpMap.vue?vue&type=template&id=2f44d178&
-var OrdpMapvue_type_template_id_2f44d178_render = function render() {
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"8c1e7290-vue-loader-template"}!./node_modules/cache-loader/dist/cjs.js??ref--13-0!./node_modules/babel-loader/lib!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./src/OrdpMap.vue?vue&type=template&id=d6b92486&
+var OrdpMapvue_type_template_id_d6b92486_render = function render() {
   var _vm = this,
     _c = _vm._self._c;
   return _c('div', {
@@ -94681,7 +94681,7 @@ var OrdpMapvue_type_template_id_2f44d178_render = function render() {
         return _vm.toggleFilter(null);
       }
     }
-  }, [_vm._l(_vm.ordsStandard.organisationTypes, function (organisationType, key) {
+  }, _vm._l(_vm.ordsStandard.organisationTypes, function (organisationType, key) {
     return _c('r-checkbox', {
       key: key,
       attrs: {
@@ -94709,32 +94709,7 @@ var OrdpMapvue_type_template_id_2f44d178_render = function render() {
         expression: "filters.organisation_types"
       }
     });
-  }), _c('r-checkbox', {
-    attrs: {
-      "value": "recycling_center",
-      "label": "recycling_center"
-    },
-    scopedSlots: _vm._u([{
-      key: "label",
-      fn: function () {
-        return [_c('span', [_c('r-icon', {
-          staticClass: "mr-1",
-          attrs: {
-            "name": "mdiMapMarker",
-            "fill": _vm.categoryColors['recycling_center']
-          }
-        }), _c('span', [_vm._v("recycling_center")])], 1)];
-      },
-      proxy: true
-    }]),
-    model: {
-      value: _vm.filters.organisation_types,
-      callback: function ($$v) {
-        _vm.$set(_vm.filters, "organisation_types", $$v);
-      },
-      expression: "filters.organisation_types"
-    }
-  })], 2) : _vm._e(), _vm.isFilterActive('ECOCHEQUES') ? _c('section-filter', {
+  }), 1) : _vm._e(), _vm.isFilterActive('ECOCHEQUES') ? _c('section-filter', {
     class: !_vm.showActiveFilters ? 'mb-6 sm:mb-12' : '',
     on: {
       "close": function ($event) {
@@ -95002,9 +94977,9 @@ var OrdpMapvue_type_template_id_2f44d178_render = function render() {
     }, [_vm._v(" " + _vm._s(_vm.$t('create_new_cta')) + " ")])];
   })], 2)], 1)], 1)], 1);
 };
-var OrdpMapvue_type_template_id_2f44d178_staticRenderFns = [];
+var OrdpMapvue_type_template_id_d6b92486_staticRenderFns = [];
 
-// CONCATENATED MODULE: ./src/OrdpMap.vue?vue&type=template&id=2f44d178&
+// CONCATENATED MODULE: ./src/OrdpMap.vue?vue&type=template&id=d6b92486&
 
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"8c1e7290-vue-loader-template"}!./node_modules/cache-loader/dist/cjs.js??ref--13-0!./node_modules/babel-loader/lib!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/ordp/CardLocation.vue?vue&type=template&id=423c9acc&
 var CardLocationvue_type_template_id_423c9acc_render = function render() {
@@ -102744,8 +102719,8 @@ const OrdpMapvue_type_script_lang_js_windowWidth = window.innerWidth;
 });
 // CONCATENATED MODULE: ./src/OrdpMap.vue?vue&type=script&lang=js&
  /* harmony default export */ var src_OrdpMapvue_type_script_lang_js_ = (OrdpMapvue_type_script_lang_js_); 
-// EXTERNAL MODULE: ./src/OrdpMap.vue?vue&type=style&index=4&id=2f44d178&prod&lang=css&
-var OrdpMapvue_type_style_index_4_id_2f44d178_prod_lang_css_ = __webpack_require__("095e");
+// EXTERNAL MODULE: ./src/OrdpMap.vue?vue&type=style&index=4&id=d6b92486&prod&lang=css&
+var OrdpMapvue_type_style_index_4_id_d6b92486_prod_lang_css_ = __webpack_require__("aabb");
 
 // CONCATENATED MODULE: ./src/OrdpMap.vue
 
@@ -102762,8 +102737,8 @@ var OrdpMapvue_type_style_index_4_id_2f44d178_prod_lang_css_ = __webpack_require
 
 var OrdpMap_component = normalizeComponent(
   src_OrdpMapvue_type_script_lang_js_,
-  OrdpMapvue_type_template_id_2f44d178_render,
-  OrdpMapvue_type_template_id_2f44d178_staticRenderFns,
+  OrdpMapvue_type_template_id_d6b92486_render,
+  OrdpMapvue_type_template_id_d6b92486_staticRenderFns,
   false,
   null,
   null,
